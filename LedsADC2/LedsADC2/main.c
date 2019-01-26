7cc7452f0eb50de43956f5004a91c19edf7d36a4
AGCG1991 <aitor_gustavo@hotmail.com>
2019-01-26 20:22:50 +0100

    Update main.c

--- LedsADC2/LedsADC2/main.c
+++ LedsADC2/LedsADC2/main.c
@@ -2,12 +2,12 @@
  * LedsADC2.c
  *
  * Created: 27/04/2018 11:51:28
- * Author : Aitor Gustavo Camacho Gutiérrez
+ * Author : 
  */ 
 
 #include <avr/io.h>
 
-//Práctica introductoria al Arduino Multipurpose UMA Shield - Parte 1 - LEDs Archivo
+//PrÃ¡ctica introductoria al Arduino Multipurpose UMA Shield - Parte 1 - LEDs Archivo
 	//Ejercicio 2
 	//Configuramos como salida el puerto DDRD (antes el B)
 	
@@ -16,7 +16,7 @@ int main(void)
 	
 	
 	DDRD =(1<<2);
-	PORTB =(1<<2); //Encendemos Led 0 (Que está conectado con PB.2)
+	PORTB =(1<<2); //Encendemos Led 0 (Que estÃ¡ conectado con PB.2)
 	
 	
 	
