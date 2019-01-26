28389a427c11b013244a87e721568f0b2615cb7d
AGCG1991 <aitor_gustavo@hotmail.com>
2019-01-26 20:24:46 +0100

    Update main.c

--- INTERRUPCIONES/INTERRUPCIONES/main.c
+++ INTERRUPCIONES/INTERRUPCIONES/main.c
@@ -2,7 +2,7 @@
  * INTERRUPCIONES.c
  *
  * Created: 03/05/2018 12:09:52
- * Author : Aitor Gustavo Camacho Gutiérrez
+ * Author : 
  */ 
 //En el ejercicio anterior hicimos el muestreo de leds mediante polling, ahora lo haremos
 //mediante interrupciones
@@ -17,22 +17,22 @@ void initADC(void) {
 	//ADEN A 1 ACTIVO CONVERSION
 	//1 << ADPS2 divisor de reloj en 16 (pag 8), dejo el resto a 0 (mirar tabla)
 	
-	//ADPS2:0 (son 3 bits de control) Preescalado del reloj para la conversión, ADC necesita entre 50 Khz a 200 Khz
-	//a mayor frecuencia , conversión mas rápida
-	//Menor frecuencia, conversión mas exacta
+	//ADPS2:0 (son 3 bits de control) Preescalado del reloj para la conversiÃ³n, ADC necesita entre 50 Khz a 200 Khz
+	//a mayor frecuencia , conversiÃ³n mas rÃ¡pida
+	//Menor frecuencia, conversiÃ³n mas exacta
 	
 	ADMUX = (1 << REFS0)  + (1 << ADLAR);
 	
 	
 	//REFS0 REFERENCIA AL VOLTAJE EXTERNO
-	// ADLAR (ALINEACIÓN RESULTADO 0 A LA DERECHA , 1 A LA IZQUIERDA)
+	// ADLAR (ALINEACIÃ“N RESULTADO 0 A LA DERECHA , 1 A LA IZQUIERDA)
 	//CON 0 A LA DERECHA
 }
 
 
 void digitalWrite(unsigned char data) {
-	PORTB = (0b00000001 & data )<<2; //Como está en el puerto B2 (hay que desplazarlo dos posiciones a la izq)
-	PORTB |= (0b00000010 & data); //Está en la segunda posicion (está en su sitio)
+	PORTB = (0b00000001 & data )<<2; //Como estÃ¡ en el puerto B2 (hay que desplazarlo dos posiciones a la izq)
+	PORTB |= (0b00000010 & data); //EstÃ¡ en la segunda posicion (estÃ¡ en su sitio)
 	PORTB |= (0b00000100 & data)>>2 ; //Esta en la posicion 2 (HAY QUE DESPLAZARLO 2 A LA DERECHA PARA QUE SE UBIQUE EN EL 0)
 	PORTD = (0b00001000 & data)<<4 ; //Estamos la posicion 3 y queremos llegar a PD7 (DESPLAZO 4 POSICIONES)
 	PORTD |= (0b00010000 & data)<<1   ; //Estamos en posicion 4 y queremos llegar pd5 (desplazo 1)
