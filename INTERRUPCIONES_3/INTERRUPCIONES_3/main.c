e0d6af145f60af6a02b2d8c6105a59d127f16fe0
AGCG1991 <aitor_gustavo@hotmail.com>
2019-01-26 20:23:51 +0100

    Update main.c

--- INTERRUPCIONES_3/INTERRUPCIONES_3/main.c
+++ INTERRUPCIONES_3/INTERRUPCIONES_3/main.c
@@ -2,7 +2,7 @@
  * INTERRUPCIONES_3.c
  *
  * Created: 
- * Author : Aitor Gustavo Camacho Gutiérrez
+ * Author :
  */ 
 
 //Acceso a las interrupciones dadas por las pulsaciones de los botones 
@@ -18,20 +18,20 @@ void initADC(void) {
 	//ADEN A 1 ACTIVO CONVERSION
 	//1 << ADPS2 divisor de reloj en 16 (pag 8), dejo el resto a 0 (mirar tabla)
 	
-	//ADPS2:0 (son 3 bits de control) Preescalado del reloj para la conversión, ADC necesita entre 50 Khz a 200 Khz
-	//a mayor frecuencia , conversión mas rápida
-	//Menor frecuencia, conversión mas exacta
+	//ADPS2:0 (son 3 bits de control) Preescalado del reloj para la conversiÃ³n, ADC necesita entre 50 Khz a 200 Khz
+	//a mayor frecuencia , conversiÃ³n mas rÃ¡pida
+	//Menor frecuencia, conversiÃ³n mas exacta
 	
 	ADMUX = (1 << REFS0)  + (1 << ADLAR);
 	//REFS0 REFERENCIA AL VOLTAJE EXTERNO
-	// ADLAR (ALINEACIÓN RESULTADO 0 A LA DERECHA , 1 A LA IZQUIERDA)
+	// ADLAR (ALINEACIÃ“N RESULTADO 0 A LA DERECHA , 1 A LA IZQUIERDA)
 	//CON 0 A LA DERECHA
 }
 
 
 void digitalWrite(unsigned char data) {
-	PORTB = (0b00000001 & data )<<2; //Como está en el puerto B2 (hay que desplazarlo dos posiciones a la izq)
-	PORTB |= (0b00000010 & data); //Está en la segunda posicion (está en su sitio)
+	PORTB = (0b00000001 & data )<<2; //Como estÃ¡ en el puerto B2 (hay que desplazarlo dos posiciones a la izq)
+	PORTB |= (0b00000010 & data); //EstÃ¡ en la segunda posicion (estÃ¡ en su sitio)
 	PORTB |= (0b00000100 & data)>>2 ; //Esta en la posicion 2 (HAY QUE DESPLAZARLO 2 A LA DERECHA PARA QUE SE UBIQUE EN EL 0)
 	PORTD = (0b00001000 & data)<<4 ; //Estamos la posicion 3 y queremos llegar a PD7 (DESPLAZO 4 POSICIONES)
 	PORTD |= (0b00010000 & data)<<1   ; //Estamos en posicion 4 y queremos llegar pd5 (desplazo 1)
@@ -52,7 +52,7 @@ ISR(PCINT0_vect) //Puerto B
 { 
 	if ((PINB & (1<< PINB4)) >> 4)
 	{
-		//Pulsamos botón A, el contador se incrementa
+		//Pulsamos botÃ³n A, el contador se incrementa
 		contador++;
 		
 		digitalWrite(contador);
@@ -79,15 +79,15 @@ int main(void)
 	DDRB = 0b00000111;
 	DDRC = 0b00011000;
 	DDRD = 0b10110000;
-	PCICR=(1<<PCIE0) ;//MASCARA (PÁGINA 8)
-	PCMSK0 =(1<<PCINT4)+(1<<PCINT3)  ;//BOTONES QUE ESTÁN EN EL 0 (PAGINA 9)
+	PCICR=(1<<PCIE0) ;//MASCARA (PÃGINA 8)
+	PCMSK0 =(1<<PCINT4)+(1<<PCINT3)  ;//BOTONES QUE ESTÃN EN EL 0 (PAGINA 9)
 	
 	EIMSK=0X01; //hABILITAR MASCARA INDIVIDUAL PARA INT0
 	EICRA=0X02;
 	EIFR=0X00;
 	
 	//Mirar pagina 8 , disyuntiva entre puerto D o E pines PCIE2 O PCIE0.
-	//El mas es una disyunción
+	//El mas es una disyunciÃ³n
 	
 	digitalWrite(0);//Ponemos a 0 los leds
 	
