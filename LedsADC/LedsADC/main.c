9eee59eba3c2929819ba6e1f61d890ed9369678c
AGCG1991 <aitor_gustavo@hotmail.com>
2019-01-26 20:23:13 +0100

    Update main.c

--- LedsADC/LedsADC/main.c
+++ LedsADC/LedsADC/main.c
@@ -2,13 +2,13 @@
  * LedsADC.c
  *
  * Created: 27/04/2018 11:38:28
- * Author : Aitor Gustavo Camacho GutiÈrrez (Alumnostr09)
+ * Author :  (Alumnostr09)
  */ 
 
 #include <avr/io.h>
-//Pr·ctica introductoria al Arduino Multipurpose UMA Shield - Parte 1 - LEDs Archivo
+//Pr√°ctica introductoria al Arduino Multipurpose UMA Shield - Parte 1 - LEDs Archivo
 //Ejercicio 1
- //El LED0 est· conectado al puerto B, patilla 2
+ //El LED0 est√° conectado al puerto B, patilla 2
  //Configura PB.2 como salida (queremos escribir en el LED) mediante DDRB
  
 int main(void)
